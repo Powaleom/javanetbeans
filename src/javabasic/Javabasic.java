59d05a08eb0538ecb57a262765510c20476314c2
Powaleom <powaleom@gmail.com>
2024-02-07 00:02:01 +0530

    basic

---
+++ src/javabasic/Javabasic.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabasic;
+
+/**
+ *
+ * @author ompowale
+ */
+public class Javabasic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a=10,b=40,c=90;
+    c=a+b;
+    System.out.println(c);
+    }
+    
+}
