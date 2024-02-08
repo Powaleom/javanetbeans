66e7f5e453d6ec2413ffd60beee85f7469d8ead7
Powaleom <powaleom@gmail.com>
2024-02-09 00:59:38 +0530

    b

---
+++ Desktop/JAVA/printing/src/printing/Printing.java
@@ -0,0 +1,20 @@
+
+package printing;
+
+
+public class Printing {
+
+    
+    public static void main(String[] args) {
+       int a=10,b=20;
+       float f=12.55f;
+       char c='A';
+       String str="hello";
+       System.out.println(a);
+       System.out.println(str);
+       System.out.println(a+" "+b);
+       
+       
+    }
+    
+}
